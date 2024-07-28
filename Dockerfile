c8befc8ec26e9ff45a2c75d1d9e64fed2137164b
unknown <zyad.sallem.007@gmail.com>
2024-07-28 22:49:06 +0300

    z

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/puppeteer/puppeteer:latest
+FROM ghcr.io/puppeteer/puppeteer:19.7.2
 
 ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
     PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
@@ -8,4 +8,4 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm ci
 COPY . .
-CMD ["node", "index.js"]
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -1,54 +1,17 @@
 const express = require("express");
-const { default: puppeteer } = require("puppeteer");
-
-const port = process.env.PORT || 4000;
+const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Render Puppeteer");
-})
+const PORT = process.env.PORT || 4000;
 
-app.get('/scrape', async (req, res) => {
+app.get("/scrape", (req, res) => {
+  scrapeLogic(res);
+});
 
-    const browser = await puppeteer.launch({
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--single-process",
-            "--no-zygote"
-        ],
-        executablePath: process.env.NODE_ENV === 'production' 
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
 
-    });
-    const page = await browser.newPage();
-
-    // Navigate the page to a URL.
-    await page.goto('https://developer.chrome.com/');
-
-    // Set screen size.
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Type into search box.
-    await page.locator('.devsite-search-field').fill('automate beyond recorder');
-
-    // Wait and click on first result.
-    await page.locator('.devsite-result-item-link').click();
-
-    // Locate the full title with a unique string.
-    const textSelector = await page
-        .locator('text/Customize and automate')
-        .waitHandle();
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-    // Print the full title.
-    res.send(fullTitle);
-    console.log('The title of this blog post is "%s".', fullTitle);
-
-    await browser.close();
-})
-
-app.listen(port, () => {
-    console.log('Server is on');
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
